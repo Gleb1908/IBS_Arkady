7df910af35d0defccd34d348a196f988265ac6ce
Gleb1908 <gleb1908@list.ru>
2021-09-29 01:40:39 +0300

    some webDriver change

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,21 +5,28 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
     protected WebDriver webDriver;
+    protected WebDriverWait webDriverWait;
     private static final String AUTOMATION_PRACTICE_URL = "https://www.rgs.ru/";
 
     @BeforeEach
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         webDriver = new ChromeDriver();
-        webDriver.get(AUTOMATION_PRACTICE_URL);
         webDriver.manage().window().maximize();
-        webDriver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        webDriver.manage().timeouts().pageLoadTimeout(60,TimeUnit.SECONDS);
+        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        webDriverWait = new WebDriverWait(webDriver,60,1000);
+
+        webDriver.get(AUTOMATION_PRACTICE_URL);
+
     }
 
     @AfterEach
